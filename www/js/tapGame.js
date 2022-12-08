0e37204178e1fcc127d4f78de72cf171f8e4b5a0
21RS093 <k21rs093@st.kyusan-u.ac.jp>
2022-12-08 10:29:56 +0900

    名前とスコアの保存

--- www/js/tapGame.js
+++ www/js/tapGame.js
@@ -7,9 +7,7 @@
 //
 
 /******************************************************/
-    // APIキーの設定
-    var APPLICATION_KEY ="967a1e7aa3007d588b7379a52987e443473a3d9b2ea97ff53062828f1476df18";
-    var CLIENT_KEY ="04fe1beb47c8061015663fb52f3925782ef0e99cd299ecbbcc24f7e23719047c";
+
 /******************************************************/
 
 // mBaaSの初期化
@@ -39,16 +37,23 @@ function startGame() {
 // 【mBaaS】データの保存
 function saveScore (name, score) {
     // **********【問題１】名前とスコアを保存しよう！**********
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // 保存先クラスを作成
+var GameScore = ncmb.DataStore("GameScore");
+// クラスインスタンスを生成
+var gameScore = new GameScore();
+// 値を設定
+gameScore.set("name", name);
+gameScore.set("score", score);
+// 保存を実施
+gameScore.save()
+.then(function (){
+// 保存に成功した場合の処理
+console.log("保存に成功しました。");
+})
+.catch(function (error){
+// 保存に失敗した場合の処理
+console.log("保存に失敗しました。エラー:" + error);
+});
     
     // ********************************************************
 }
